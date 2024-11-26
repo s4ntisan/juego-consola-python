62918db9395334b7506db9ff23efb34b45f64524
s4ntisan <carsantiagosanchez@gmail.com>
2024-11-26 13:21:54 -0300

    Delete Herencia directory

--- Herencia/villano.py
+++
@@ -1,9 +0,0 @@
-from Herencia.personaje import Personaje
-
-class Villano(Personaje):
-    def __init__(self, nombre, salud, habilidad):
-        super().__init__(nombre, salud)
-        self.habilidad = habilidad
-
-    def mostrar_habilidad(self):
-        print(f"{self.nombre} tiene la habilidad de {self.habilidad}")
